ecd4408c00ea1c5e883ea93f1825e449ab0c55e1
Gayatri <gayatrinaik95@yahoo.com>
2022-01-12 14:52:25 +0530

    Table Of 7th

---
+++ Web-Automation/src/com/sgtesting/javaprograms/Sample9.java
@@ -0,0 +1,15 @@
+package com.sgtesting.javaprograms;
+
+public class Sample9 {
+	
+		public static void main(String args[])
+		{
+		for(int i=10;i>=1;i--)
+		{
+		 System.out.println("7 * "+i+" = "+(i*7));
+		}
+		}
+		
+	}
+
+
